64dae3e8faa10a381dbcc5a7231ea4e3910939c2
Sycoder <foxkeys@mail.ru>
2013-02-10 00:30:57 +0300

    Daily commit

---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+	/**
+	 * Created by JetBrains PhpStorm.
+	 * Author: Fox foxkeys@gmail.com
+	 * Date Time: 06.02.2013 11:32
+	 */
+	error_reporting( E_ALL );
+	ini_set( "display_errors", 1 );
+
+
+	require_once( 'configFox.php' );
+	require_once( 'autoloader.php' );
+	/*
+		$system = new system( game::DB() );
+		for ( $i = 0; $i < 1000; $i++ ) {
+			echo $system->idStarClass( game::starClasses()->getRandom()->idStarClass() ) . PHP_EOL;
+		}
+	*/
+	//game::galaxyGenerator()->generateGalaxy( 'Test', 1 );
+
+	$universe = universe::createFromDB( 1, game::DB() );
+	$galaxyTemplate = galaxyTemplate::createFromDB( 1, game::DB() );
+	$currentUser = game::auth()->currentUser();
+	if ( $currentUser->galaxyCreateLimit() > 0 ) {
+		$universe->createNewGalaxy( $galaxyTemplate, 'New test galaxy', 0, 0, 100 );
+		$currentUser->galaxyCreateLimit( $currentUser->galaxyCreateLimit() - 1 );
+		$currentUser->save();
+	} else {
+		throw new Exception( fConst::E_CANT_CREATE_MORE_GALAXIES );
+	}
+
+
+	//var_dump( $universe );
+	//var_dump( $galaxyTemplate );
+
+//	$planets = planet::selectByIdSystem( 1, game::DB() );
